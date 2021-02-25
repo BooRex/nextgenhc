82e8424d4e309a90d244c26f4dc959fe9bafbefa
Oleh Brytvin <boorex21997@gmail.com>
2021-02-25 19:42:27 +0200

    Add ignore file, add output and input

---
+++ index.js
@@ -0,0 +1,5 @@
+import { parse } from './input.js';
+import { stringify } from './output.js';
+
+parse();
+stringify();
---
+++ output.js
@@ -0,0 +1,1 @@
+export const stringify = () => console.log('---', 'stringify');
