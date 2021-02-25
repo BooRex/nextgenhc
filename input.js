46a248209735801064e42ce99e94843a6fc67741
Oleh Brytvin <boorex21997@gmail.com>
2021-02-25 22:32:53 +0200

    optimize input

--- input.js
+++ input.js
@@ -6,30 +6,28 @@ export const getTime = () => {
   return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}-${date.getMilliseconds()}`;;
 }
 
-const convert = name => {
-  const raw = fs.readFileSync(`./files/${ name }.txt`).toString().split('\n');
-  console.log('file parsed', getTime());
-  const infoArr = raw[0].split(' ');
+const first = (raw, streetsCount) => {
+  console.log('start first', getTime());
+  return raw.map((i, idx) => {
+    if (idx > 1 && idx <=streetsCount) {
+      const rawStreet = i.split(' ');
 
-  const duration = parseInt(infoArr[0]);
-  const intersectionsCount = parseInt(infoArr[1]);
-  const streetsCount = parseInt(infoArr[2]);
-  const carsCount = parseInt(infoArr[3]);
-  const bonusPoints = parseInt(infoArr[4]);
+      return {
+        intersectionA: parseInt(rawStreet[0]),
+        intersectionB: parseInt(rawStreet[1]),
+        name: rawStreet[2],
+        time: parseInt(rawStreet[3])
+      };
+    } else {
+      return null;
+    }
+  }).filter(i => i != null);
+}
 
-  let streetsArr = [];
+const second = (raw, streetsCount, carsCount) => {
   let carsArr = [];
 
-  for (let i = 1; i <= streetsCount; i++) {
-    const rawStreet = raw[i].split(' ');
-
-    streetsArr = [...streetsArr, {
-      intersectionA: parseInt(rawStreet[0]),
-      intersectionB: parseInt(rawStreet[1]),
-      name: rawStreet[2],
-      time: parseInt(rawStreet[3])
-    }];
-  }
+  console.log('start second', getTime());
   for (let m = streetsCount + 1; m <= streetsCount + carsCount; m++) {
     const rawCar = raw[m].split(' ');
     const streetCount = parseInt(rawCar[0]);
@@ -43,7 +41,25 @@ const convert = name => {
       streetCount,
       streets: streetsArrNew,
     }];
+
   }
+  console.log('finish second', getTime());
+  return carsArr;
+}
+
+const convert = name => {
+  const raw = fs.readFileSync(`./files/${ name }.txt`).toString().split('\n');
+  console.log('file parsed', getTime());
+  const infoArr = raw[0].split(' ');
+
+  const duration = parseInt(infoArr[0]);
+  const intersectionsCount = parseInt(infoArr[1]);
+  const streetsCount = parseInt(infoArr[2]);
+  const carsCount = parseInt(infoArr[3]);
+  const bonusPoints = parseInt(infoArr[4]);
+
+  let streetsArr = first(raw, streetsCount);
+  let carsArr = second(raw, streetsCount, carsCount);
 
   return {
     name,
