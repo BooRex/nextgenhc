5f24520f3ac769acb5194a221b8b020a1bfd0361
Bogdan Onischenko <dever4eg@gmail.com>
2021-02-25 20:56:46 +0200

    Add calc firest verasdbvas

--- calc.js
+++ calc.js
@@ -1,1 +1,41 @@
-export const calc = () => console.log('---', 'calc');
+export const calc = (input) => {
+    const intersectionStreetsMap = {}
+
+    input.streets.forEach((street) => {
+        if (!intersectionStreetsMap[street.intersectionA])
+            intersectionStreetsMap[street.intersectionA] = []
+
+        intersectionStreetsMap[street.intersectionA].push(street.name)
+
+        if (!intersectionStreetsMap[street.intersectionB])
+            intersectionStreetsMap[street.intersectionB] = []
+    })
+
+    console.log(intersectionStreetsMap)
+
+    const schedules = [];
+
+    Object.entries(intersectionStreetsMap).forEach(([id, streets]) => {
+        const scheduleItems = []
+
+        streets.forEach(street => {
+            scheduleItems.push({
+                streetName: street.name,
+                duration: 1
+            })
+        })
+
+        schedules.push(
+            {
+                intersectionId: id,
+                streetsCount: scheduleItems.length,
+                scheduleItems
+            }
+        )
+    })
+
+    return {
+        scheduleCount: schedules.length,
+        schedules
+    }
+}
